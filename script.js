a25de4051e7412baadd8fd7d4438ba12474613da
rhass14 <rebecca.hass91@googlemail.com>
2024-04-26 17:26:28 +0200

    fix button display

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 const btn = document.querySelector("button");
+
 function toggleDarkMode() {
   document.body.classList.toggle("body-dark");
   btn.classList.toggle("button-dark");
+
+if (document.body.classList.contains("body-dark")) {
+  btn.innerHTML = "Night";
+} else {
+  btn.innerHTML = "Day";
+}
 }
 
 btn.addEventListener("click", toggleDarkMode);
